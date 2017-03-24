d4322c56a347a4b74fa51539efe2658e0192edc2
Bilal Khan <Bilalkhanten@gmail.com>
2017-03-24 11:07:05 -0700

    Create Link.cpp

---
+++ Link.cpp
@@ -0,0 +1,141 @@
+#include "list.h"
+
+void createList(List &L) {
+    /**
+    * FS : first(L) diset Nil
+    */
+    //-------------your code here-------------
+    // NIM : 
+    
+
+    //----------------------------------------
+}
+
+address alokasi(infotype x) {
+    /**
+    * FS : mengembalikan elemen list baru dengan info = x, next elemen = Nil
+    */
+
+    address P;
+    //-------------your code here-------------
+    // NIM : 
+
+
+    //----------------------------------------
+    return P;
+}
+
+void dealokasi(address &P) {
+    /**
+    * FS : menghapus elemen yang ditunjuk oleh P (delete)
+    */
+    //-------------your code here-------------
+    // NIM : 
+
+	
+    //----------------------------------------
+}
+
+void insertFirst(List &L, address P) {
+    /**
+    * IS : List L mungkin kosong
+    * FS : elemen yang ditunjuk P menjadi elemen pertama pada List L
+    */
+    //-------------your code here-------------
+    // NIM : 
+	
+
+    //----------------------------------------
+}
+
+void insertLast(List &L, address P) {
+    /**
+    * IS : List L mungkin kosong
+    * FS : elemen yang ditunjuk P menjadi elemen terakhir pada List L
+    */
+    //-------------your code here-------------
+    // NIM : 
+    
+	
+    //----------------------------------------
+}
+
+address findElm(List L, infotype x) {
+    /**
+    * IS : List L mungkin kosong
+    * FS : mengembalikan elemen dengan info.ID = x.ID,
+           mengembalikan Nil jika tidak ditemukan
+    */
+
+    address P;
+    //-------------your code here-------------
+    // NIM : 
+    
+	
+    //----------------------------------------
+    return P;
+}
+
+void deleteFirst(List &L, address &P) {
+    /**
+    * IS : List L mungkin kosong
+    * FS : elemen pertama di dalam List L dilepas dan disimpan/ditunjuk oleh P
+    */
+    //-------------your code here-------------
+    // NIM : 
+
+	
+	
+    //----------------------------------------
+}
+
+void deleteLast(List &L, address &P) {
+    /**
+    * IS : List L mungkin kosong
+    * FS : elemen tarakhir di dalam List L dilepas dan disimpan/ditunjuk oleh P
+    */
+    //-------------your code here-------------
+    // NIM : 
+
+	
+
+    //----------------------------------------
+}
+
+void printInfo(List L) {
+    /**
+    * FS : menampilkan info seluruh elemen list L
+    */
+    //-------------your code here-------------
+    // NIM : 
+
+	
+    //----------------------------------------
+}
+
+
+void insertAfter(address Prec, address P) {
+    /**
+    * IS : Prec dan P tidak NULL
+    * FS : elemen yang ditunjuk P menjadi elemen di belakang elemen yang
+    *      ditunjuk pointer Prec
+    */
+    //-------------your code here-------------
+    // NIM : 
+
+	
+    //----------------------------------------
+
+}
+void deleteAfter(address Prec, address &P) {
+    /**
+    * IS : Prec tidak NULL
+    * FS : elemen yang berada di belakang elemen Prec dilepas
+    *      dan disimpan/ditunjuk oleh P
+    */
+    //-------------your code here-------------
+    // NIM : 
+    
+	
+    //----------------------------------------
+}
